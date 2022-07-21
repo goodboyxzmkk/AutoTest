077cf6b567bed337e38a5708b4923897a4304155
ty <346315670@qq.com>
2022-07-21 18:14:18 +0800

    优化

---
+++ myTest/src/main/java/com/ych/test/mainClass.java
@@ -0,0 +1,13 @@
+package com.ych.test;
+
+import static com.ych.test.RandomUtils.getRandomStr;
+
+/**
+ * 用于生成jar包，不可删除
+ */
+public class mainClass {
+    public static void main(String[] args) {
+        System.out.println(getRandomStr(5));
+
+    }
+}
